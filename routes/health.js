b619ea0550bf8d9840a1df1db9d09a188c083309
Hilburger <B058487@win.cs.nuernberger.net>
2018-12-19 13:01:46 +0100

    V2

--- routes/health.js
+++ routes/health.js
@@ -3,17 +3,27 @@ var router = express.Router();
 var path = require('path');
 var fs = require('fs');
 var plzPath = path.join(__dirname, '../plz.txt');
-/* GET home page. */
+var cityPath = path.join(__dirname, '../city.txt');
+
 router.get('/', function(req, res/*, next*/) {
-  const fsize = fs.statSync(plzPath).size;
-  if (fsize>0)
+  var fsizeplz=0;
+  var fsizecity=0;
+  try{  
+   fsizeplz = fs.statSync(plzPath).size;
+   fsizecity = fs.statSync(cityPath).size;
+  }
+  catch (err){
+  //ignore size will be 0
+  }
+  if (fsizeplz>0 & fsizecity>0)
   {
-   res.send('{"outcome":"UP","checks":[{"name":"PLZ-File","state":"UP"}]}');
+   res.send('{"outcome":"UP","checks":[{"name":"plz-city-files","state":"UP"}]}');
   }
   else  
   {
-    res.send('{"outcome":"DOWN","checks":[{"name":"PLZ-File","state":"DOWN"}]}');
+    res.send('{"outcome":"DOWN","checks":[{"name":"plz-city-files","state":"DOWN"}]},');
   }
 });
 
 module.exports = router;
+
